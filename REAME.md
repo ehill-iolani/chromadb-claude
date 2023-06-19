9fec8dc030ba77028d15c23b4535d84761134168
Ethan Hill <ehill@iolani.org>
2023-06-19 13:10:24 -1000

    initial commit getting base set up

---
+++ REAME.md
@@ -0,0 +1,9 @@
+# Testing chromadb + claude
+
+To use this tool you must set up the following environment and install the required dependencies:
+```
+git clone #insert reponame
+python3 -m venv env
+.\env\Scripts\activate
+pip install -r requirements.txt
+```
---
+++ pdf2txt.py
@@ -0,0 +1,20 @@
+
+# importing required modules
+from PyPDF2 import PdfReader
+import os
+
+# path of the PDF file
+path = "./pdfs/"
+dir_list = os.listdir(path)
+
+# convert all files in the pdfs directory to text
+for file in dir_list:
+    if file.endswith(".pdf"):
+        # creating a pdf reader object
+        reader = PdfReader(path+file)
+        # save to a text file for later use
+        # copy the path where the script and pdf is placed
+        with open('./text/'+file[:-4]+'.txt', 'w', encoding='utf-8') as f:
+            for page in reader.pages:
+                text = page.extract_text()
+                f.write(text)
